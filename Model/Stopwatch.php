926146a9d09f394445fcfc1c1b133bc362573ec3
Henry <hwong@earls.ca>
2014-12-22 06:35:03 -0800

    FUL-306: change the getEvent method in order to make it similar to the repository

--- Model/Stopwatch.php
+++ Model/Stopwatch.php
@@ -15,7 +15,6 @@ class Stopwatch extends \Symfony\Component\Stopwatch\Stopwatch implements Stopwa
      */
     public function getFinishTime($name)
     {
-        /*
         $seconds = round($this->getEvent($name)->getDuration() / 1000, 0);
 
         $d1 = new \DateTime();
@@ -23,11 +22,18 @@ class Stopwatch extends \Symfony\Component\Stopwatch\Stopwatch implements Stopwa
         $d2->add(new \DateInterval("PT{$seconds}S"));
 
         $duration = $d2->diff($d1);
-        */
-
-        $duration = 10;
 
         return $duration;
     }
 
+    public function getEvent($name)
+    {
+        $events = end($this->activeSections);
+
+        if(!(isset($events[$name]))){
+            throw new \LogicException(sprintf("Error, %s event does not exists", $name));
+        }
+
+        return $events[$name];
+    }
 }
