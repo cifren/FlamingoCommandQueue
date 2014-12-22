3dc9febc2dd58ad00c1118c688bab1d8dfc3a4ee
Henry <hwong@earls.ca>
2014-12-22 05:52:16 -0800

    FUL-306: Overide getFinishTime in order to disable it

--- Model/Stopwatch.php
+++ Model/Stopwatch.php
@@ -15,6 +15,7 @@ class Stopwatch extends \Symfony\Component\Stopwatch\Stopwatch implements Stopwa
      */
     public function getFinishTime($name)
     {
+        /*
         $seconds = round($this->getEvent($name)->getDuration() / 1000, 0);
 
         $d1 = new \DateTime();
@@ -22,19 +23,11 @@ class Stopwatch extends \Symfony\Component\Stopwatch\Stopwatch implements Stopwa
         $d2->add(new \DateInterval("PT{$seconds}S"));
 
         $duration = $d2->diff($d1);
+        */
+
+        $duration = 10;
 
         return $duration;
     }
 
-    public function getEvent($name)
-    {
-        $current = end($this->activeSections);
-        $events = $this->getSectionEvents($current->getId());
-        if(isset($events[$name])){
-            
-            return $events[$name];
-        }
-        
-        throw new \Exception('ERROR: Event does not exists');
-    }
 }
