295d5c88f362bace8d02f500c2dc01ae1801ea0a
francis <cloud9+cifren8x@gmail.com>
2016-06-13 09:11:53 +0000

    add test

--- Tests/Model/FlgCommandOptionTest.php
+++ Tests/Model/FlgCommandOptionTest.php
@@ -2,23 +2,25 @@
 
 namespace Earls\FlamingoCommandQueueBundle\Tests\Model;
 
+use Earls\FlamingoCommandQueueBundle\Model\FlgCommandOption;
+
 class FlgCommandOptionTest extends \PHPUnit_Framework_TestCase
 {
     public function testCreate()
     {
         $item = new FlgCommandOption();
-        
+
         $item->setGroupName('setGroupName');
-        $this->assertEquals('setGroupName',$this->get());
+        $this->assertEquals('setGroupName', $this->get());
         $item->setUniqueId('unique');
-        $this->assertEquals('unique',$this->getUniqueId());
+        $this->assertEquals('unique', $this->getUniqueId());
         $item->setMaxPendingInstance(12441);
-        $this->assertEquals(12441,$this->getMaxPendingInstance());
+        $this->assertEquals(12441, $this->getMaxPendingInstance());
         $item->setPendingLapsTime(325223);
-        $this->assertEquals(325223,$this->getPendingLapsTime());
+        $this->assertEquals(325223, $this->getPendingLapsTime());
         $item->setArchiveEnable(true);
-        $this->assertEquals(true,$this->getArchiveEnable());
+        $this->assertEquals(true, $this->getArchiveEnable());
         $item->setWatchScriptReferences('setWatchScriptReferences');
-        $this->assertEquals('setWatchScriptReferences',$this->getWatchScriptReferences());
+        $this->assertEquals('setWatchScriptReferences', $this->getWatchScriptReferences());
     }
 }
